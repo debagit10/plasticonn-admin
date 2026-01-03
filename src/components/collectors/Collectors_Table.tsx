92c439610090788f5fb64e7e0a2ae92b9066b639
Josiah Elias <josiahelias11@gmail.com>
2026-01-03 18:38:56 +0100

    feat: add Collectors_Table component with search and filter functionality

---
+++ src/components/collectors/Collectors_Table.tsx
@@ -0,0 +1,249 @@
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TablePagination,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+interface TableProps {
+  search: string;
+  filter: string;
+}
+
+const Collectors_Table: React.FC<TableProps> = ({ search, filter }) => {
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+
+  const head = ["Name", "Email", "Status", "Joined", "Actions"];
+
+  const rows = [
+    {
+      name: "John Doe",
+      email: "john@email.com",
+      status: "active",
+      createdAt: "2024-01-10",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane1@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane2@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@4email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@e5mail.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@em6ail.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@ema7il.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@emai8l.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@email9.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane10@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane11@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane12@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane13@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane15@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane14@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane18@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane16@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane17@email.com",
+      status: "suspended",
+      createdAt: "2024-01-12",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane19@email.com",
+      status: "active",
+      createdAt: "2024-01-12",
+    },
+  ];
+
+  const filteredRows = rows.filter((row) => {
+    const matchesSearch =
+      row.name.toLowerCase().includes(search.toLowerCase()) ||
+      row.email.toLowerCase().includes(search.toLowerCase());
+
+    const matchesFilter = filter === "all" || row.status === filter;
+
+    return matchesSearch && matchesFilter;
+  });
+
+  console.log(filter);
+
+  const paginatedRows = filteredRows.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
+  const handleChangePage = (_: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <div className="mt-6">
+      <TableContainer
+        sx={{
+          maxHeight: 500,
+          overflowY: "auto",
+          borderRadius: "18px",
+          borderWidth: "0.5px",
+          borderColor: "#1A1A1ACC",
+        }}
+      >
+        <Table
+          stickyHeader
+          sx={{
+            minWidth: 650,
+            backgroundColor: "#FAFAFA",
+          }}
+        >
+          <TableHead sx={{ backgroundColor: "#EAF5F2" }}>
+            <TableRow>
+              {head.map((item) => (
+                <TableCell
+                  key={item}
+                  sx={{
+                    py: 3,
+                    px: 4,
+                    backgroundColor: "#EAF5F2",
+                  }}
+                >
+                  <Typography fontWeight={700} fontSize={18} color="#1A1A1AB2">
+                    {item}
+                  </Typography>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {paginatedRows.length ? (
+              paginatedRows.map((row) => (
+                <TableRow
+                  key={row.email}
+                  sx={{
+                    fontWeight: 400,
+                    fontSize: 18,
+                    color: "#1A1A1A",
+                    backgroundColor: "#FAFAFA",
+                  }}
+                >
+                  <TableCell sx={{ px: 4 }}>{row.name}</TableCell>
+                  <TableCell sx={{ px: 4 }}>{row.email}</TableCell>
+                  <TableCell sx={{ px: 4 }}>{row.status}</TableCell>
+                  <TableCell sx={{ px: 4 }}>{row.createdAt}</TableCell>
+                  <TableCell sx={{ px: 4 }}>...</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={5} align="center">
+                  No data found
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TablePagination
+        rowsPerPageOptions={[10, 25]}
+        component="div"
+        count={filteredRows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
+  );
+};
+
+export default Collectors_Table;
--- src/pages/Collectors.tsx
+++ src/pages/Collectors.tsx
@@ -1,12 +1,105 @@
+import { InputAdornment, MenuItem, TextField } from "@mui/material";
 import Pages from "../container/Pages";
+import { IoSearchOutline } from "react-icons/io5";
+import { useState } from "react";
+import Collectors_Table from "../components/collectors/Collectors_Table";
 
 const Collectors = () => {
+  const [value, setValue] = useState("all");
+  const [search, setSearch] = useState("");
+
   return (
     <Pages
       page="Collectors Management"
       helperText="Manage and oversee collection activities."
     >
-      Collectors
+      <div className=" px-5 mt-6">
+        <div className="flex gap-2">
+          <TextField
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search by name or email..."
+            variant="outlined"
+            size="small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IoSearchOutline color="#1A1A1AB2" />
+                </InputAdornment>
+              ),
+            }}
+            sx={{
+              width: 1019,
+
+              // overall height
+              "& .MuiOutlinedInput-root": {
+                height: 50,
+                borderRadius: "12px",
+                backgroundColor: "#FAFAFA",
+
+                // default border
+                "& fieldset": {
+                  borderColor: "#1A1A1A",
+                  borderWidth: "0.2px",
+                },
+
+                // focused
+                "&.Mui-focused fieldset": {
+                  borderColor: "#1A1A1A",
+                  borderWidth: "0.2px",
+                },
+              },
+
+              // input text
+              "& input": {
+                padding: "10px 12px",
+                fontSize: 14,
+              },
+            }}
+          />
+
+          <TextField
+            select
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Select period"
+            sx={{
+              width: 230,
+
+              // overall height
+              "& .MuiOutlinedInput-root": {
+                height: 48,
+                borderRadius: "12px",
+                backgroundColor: "#FAFAFA",
+
+                // default border
+                "& fieldset": {
+                  borderColor: "#1A1A1A",
+                  borderWidth: "0.2px",
+                },
+
+                // focused
+                "&.Mui-focused fieldset": {
+                  borderColor: "#1A1A1A",
+                  borderWidth: "0.2px",
+                },
+              },
+
+              // input text
+              "& input": {
+                padding: "10px 12px",
+                fontSize: 14,
+              },
+            }}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="suspended">Suspended</MenuItem>
+            <MenuItem value="active">Active</MenuItem>
+          </TextField>
+        </div>
+
+        <Collectors_Table search={search} filter={value} />
+      </div>
     </Pages>
   );
 };
